e63386e4682e91533abdb7b262563526a4a57b0e
Brendan Smith <795924@seq.org>
2019-03-05 08:51:06 -0800

    vsdg

--- Labs/Lab 2.27Acceleration and Attraction/ball2.js
+++ Labs/Lab 2.27Acceleration and Attraction/ball2.js
@@ -5,64 +5,56 @@
 */
 
 function Ball(location, velocity, radius, col){
-  // Instance Variables
-  this.loc = location;
-  this.vel = velocity;
-  this.rad = radius;
-  this.col = col;
-
+  // Instance variables
+   this.loc = location;
+   this.vel = velocity;
+   this.rad = radius;
+   this.col = col;
   // This function calls other functions
   this.run = function(){
     this.checkEdges();
     this.update();
     this.render();
   }
+  // This function changes the location of the ball
+  // by adding speed to x and y
+   this.update = function(){
+    this.loc.add(this.vel)
+   }
 
-  this.update = function(){
-    if(this !== chaser){
-      //if this is less than 250 away from ball
-      //attract:: accelerate towards ball
-      var d = this.loc.dist(chaser.loc)
-      if(d<450){
-        var attForce = p5.Vector.sub(chaser.loc, this.loc);
-        attForce.normalize();
-        attForce.mult(.01);
-        this.vel.add(attForce)
-      }
-      //if this is less than 85 away from ball
-      //repel:: accelerate away from ball
-      if(d < 85){
-        var repForce = p5.Vector.sub( this.loc, chaser.loc,);
-        repForce.normalize();
-        repForce.mult(.75);
-        this.vel.add(repForce)
-      }
-    }
-    this.vel.limit(3);
-    this.loc.add(this.vel);
+   //checkEdges() reverses speed when the ball touches an edge
+   this.checkEdges = function(){
+     if(this.loc.x < 0) this.vel.x = -this.vel.x;
+     if(this.loc.x > width) this.vel.x = -this.vel.x;
+     if(this.loc.y < 0) this.vel.y = -this.vel.y;
+     if(this.loc.y > height) this.vel.y = -this.vel.y;
 
-  }
+   }
 
-  //checkEdges() reverses speed when the ball touches an edge
-  this.checkEdges = function(){
-    if(this.loc.x < 0) this.vel.x = -this.vel.x
-    if(this.loc.x > width) this.vel.x = -this.vel.x
-    if(this.loc.y < 0) this.vel.y = -this.vel.y
-    if(this.loc.y > height) this.vel.y = -this.vel.y
+    // render() draws the ball at the new location
+    this.render = function(){
+       fill(100,700,225);
+       if(this.loc.x<400 && this.loc.y>400 ){
+       stroke(0,0,random(100,200)+this.loc.dist(this.vel))
+       strokeWeight(random(1,5));
+       line(this.loc.x, this.loc.y, 10, 40);
+     }
+     if(this.loc.x>400 && this.loc.y>400 ){
+     stroke(random(100,200)+this.loc.dist(this.vel),0,0)
+     strokeWeight(random(1,5));
+     rect(this.loc.x, this.loc.y, 10, 40);
+   }
+     if(this.loc.x<400 && this.loc.y<400){
+       stroke(random(100,200)+this.loc.dist(this.vel),random(100,200)+this.loc.dist(this.vel),random(100,200)+this.loc.dist(this.vel))
+       strokeWeight(random(1,5));
+       line(this.loc.x, this.loc.y, 10, 40);
+     }
+     if(this.loc.x>400 && this.loc.y<400){
+     stroke(0,random(100,200)+this.loc.dist(this.vel),0)
+     strokeWeight(random(1,5));
+     line(this.loc.x, this.loc.y, 10, 40);
+   }
+ }
 
-  }
-  // render () draws the ball at the new location
-  this.render = function (){
-    fill(this.col);
-    ellipse(this.loc.x,this.loc.y, this.rad, this.rad)
-    stroke(150, 241, 220);
-    line(this.loc.x, this.loc.y,balls[0].loc.x, balls [0].loc.y);
-    stroke(150, 241, 220);
-    line(this.loc.x, this.loc.y,balls[0].loc.x, balls [0].loc.y);
-    stroke(150, 241, 220);
-    line(this.loc.x, this.loc.y,balls[0].loc.x, balls [0].loc.y);
-    stroke(150, 241, 220);
-    line(this.loc.x, this.loc.y,balls[0].loc.x, balls [0].loc.y);
-  }
-}
-var orbiter
+
+ }
